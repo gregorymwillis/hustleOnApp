4afb905b0bb753f8d1dabd1bdc0bfd14bf390a90
Greg Willis <gregorymwillis@gmail.com>
2017-12-03 19:22:14 -0700

    Initial Commit

---
+++ hustle-mode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hustle-mode
+//
+//  Created by Greg Willis on 12/3/17.
+//  Copyright © 2017 Greg Willis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
